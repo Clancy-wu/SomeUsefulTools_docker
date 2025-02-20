57e995103801697edb4ab1ed1d3f025195c891a5
Kang Wu <53364840+Clancy-wu@users.noreply.github.com>
2025-02-20 20:08:24 +0800

    Add files via upload

---
+++ run_fmriprep_nobbr_derive.sh
@@ -0,0 +1,21 @@
+!#/bin/bash
+for_each -nthreads 6 BIDS/sub-* : docker run -ti --rm \
+    -v /home/clancy/ssd:/work \
+    -u $(id -u):$(id -g) \
+    -v /home/clancy/TemplateFlow:/opt/templateflow \
+    -e TEMPLATEFLOW_HOME=/opt/templateflow \
+    nipreps/fmriprep:24.1.1 \
+    /work/BIDS/ /work/fmriprep/ participant --participant-label PRE \
+	--skip_bids_validation \
+	--ignore fieldmaps \
+	-w /work/fmriprep_work \
+	--nthreads 1 --omp-nthreads 2 \
+	--output-spaces MNI152NLin6Asym:res-2 \
+	--force-no-bbr \
+	--fs-license-file /work/license.txt \
+	--fs-subjects-dir /work/free_dir \
+	--output-layout bids \
+	--resource-monitor \
+	--notrack \
+	--stop-on-first-crash
+
